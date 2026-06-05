9529db84acd030c1450d46b65122d4b0cefcd42c
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-06-05 15:01:55 +0500

    top

--- src/components/top.jsx
+++ src/components/top.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Top = () => {
   return (
-    <div>Top</div>
-  )
+    <div>
+      <div className=" flex p-5 justify-between items-end">
+        <div>
+          <h1 className=' text-3xl font-bold'>Мини-CRUD (классная работа)</h1>
+          <p className='text-gray-500'>Поиск • Модальные окна • Карточки</p>
+        </div>
+        <button className='w-40 h-12 bg-black text-white rounded-2xl cursor-pointer'>+ Добавить</button> 
+      </div>
+    </div>
+  );
 }
 
 export default Top

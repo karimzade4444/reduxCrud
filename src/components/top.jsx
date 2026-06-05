4bda12c66f463b80b50891a2490a89dba0b34c2e
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-06-05 15:05:12 +0500

    middle

--- src/components/top.jsx
+++ src/components/top.jsx
@@ -3,12 +3,23 @@ import React from 'react'
 const Top = () => {
   return (
     <div>
-      <div className=" flex p-5 justify-between items-end">
+      <div className=" flex p-10 justify-between items-end">
         <div>
-          <h1 className=' text-3xl font-bold'>Мини-CRUD (классная работа)</h1>
-          <p className='text-gray-500'>Поиск • Модальные окна • Карточки</p>
+          <h1 className=" text-3xl font-bold">Мини-CRUD (классная работа)</h1>
+          <p className="text-gray-500">Поиск • Модальные окна • Карточки</p>
         </div>
-        <button className='w-40 h-12 bg-black text-white rounded-2xl cursor-pointer'>+ Добавить</button> 
+        <button className="w-40 h-12 bg-black text-white rounded-2xl cursor-pointer">
+          + Добавить
+        </button>
+      </div>
+      <div className='p-10'>
+        <input
+          type="search"
+          name=""
+          placeholder="Поиск"
+          id=""
+          className="w-100 h-12 border-black border-2 rounded-2xl pl-5"
+        />
       </div>
     </div>
   );
